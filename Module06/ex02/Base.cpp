335bdfb0e8e1c0092f64bc413c7441c6b0dc4649
esergeev <polsraka@gmail.com>
2026-04-01 20:50:42 +0200

    ex02

---
+++ Module06/ex02/Base.cpp
@@ -0,0 +1,16 @@
+#include "Base.hpp"
+
+Base * generate(void){
+
+	if((std::rand() % 3) + 1 == )
+		std::cout << "class A" << std::endl;
+}
+
+void identify(Base* p){
+
+}
+
+void identify(Base& p){
+
+
+}
---
+++ Module06/ex02/main.cpp
@@ -0,0 +1,7 @@
+#include "Base.hpp"
+
+int main(){
+
+srand(time(0));
+
+}
